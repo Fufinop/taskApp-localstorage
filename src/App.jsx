20c215c60ff3c1a222e43f38d50ff5eb2cc46bbc
Ernesto De la Rosa <ernestodelazamora@gmail.com>
2023-01-27 01:39:32 -0600

    added elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import { useState } from 'react';
+import TaskCreator from './components/TaskCreator';
+
 function App() {
-	return <div className=''></div>;
+	const [tasksItems, setTasksItems] = useState([
+		{ name: 'Task 1', done: false },
+		{ name: 'Task 2', done: false },
+		{ name: 'Task 3', done: false },
+	]);
+
+	function createNewTask(taskName) {
+		if (!tasksItems.find(task => task.name === taskName)) {
+			setTasksItems([...tasksItems, { name: taskName, done: false }]);
+		}
+	}
+
+	return (
+		<div>
+			<TaskCreator createNewTask={createNewTask} />
+
+			<table>
+				<thead>
+					<tr>
+						<th>Tasks</th>
+					</tr>
+				</thead>
+				<tbody>
+					{tasksItems.map(task => (
+						<tr key={task.name}>
+							<td>{task.name}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default App;
