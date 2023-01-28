90c63b88b497e3120c2485418c4677e3b63fb2e1
Ernesto De la Rosa <ernestodelazamora@gmail.com>
2023-01-28 11:49:07 -0600

    reformatting of the document

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TaskCreator from './components/TaskCreator';
+import TaskTable from './components/TaskTable';
+import VisibilityControl from './components/VisibilityControl';
 
 function App() {
-	const [tasksItems, setTasksItems] = useState([
-		{ name: 'Task 1', done: false },
-		{ name: 'Task 2', done: false },
-		{ name: 'Task 3', done: false },
-	]);
+	const [tasksItems, setTasksItems] = useState([]);
+	const [showCompleted, setShowCompleted] = useState(false);
 
 	function createNewTask(taskName) {
 		if (!tasksItems.find(task => task.name === taskName)) {
@@ -14,24 +13,47 @@ function App() {
 		}
 	}
 
-	return (
-		<div>
-			<TaskCreator createNewTask={createNewTask} />
+	const toggleTask = task => {
+		setTasksItems(
+			tasksItems.map(t => (t.name === task.name ? { ...t, done: !t.done } : t)),
+		);
+	};
 
-			<table>
-				<thead>
-					<tr>
-						<th>Tasks</th>
-					</tr>
-				</thead>
-				<tbody>
-					{tasksItems.map(task => (
-						<tr key={task.name}>
-							<td>{task.name}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
+	const clearTasks = () => {
+		setTasksItems(tasksItems.filter(task => !task.done));
+		setShowCompleted(false);
+	};
+
+	useEffect(() => {
+		const data = localStorage.getItem('tasks');
+
+		if (data) {
+			setTasksItems(JSON.parse(data));
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem('tasks', JSON.stringify(tasksItems));
+	}, [tasksItems]);
+
+	return (
+		<div className='h-screen bg-gray-700 text-white'>
+			<div className='container mx-auto px-4 sm:px-4 md:w-1/3 '>
+				<TaskCreator createNewTask={createNewTask} />
+				<TaskTable tasks={tasksItems} toggleTask={toggleTask} />
+				<VisibilityControl
+					isChecked={showCompleted}
+					setShowCompleted={checked => setShowCompleted(checked)}
+					cleanTasks={clearTasks}
+				/>
+				{showCompleted === true && (
+					<TaskTable
+						tasks={tasksItems}
+						toggleTask={toggleTask}
+						showCompleted={showCompleted}
+					/>
+				)}
+			</div>
 		</div>
 	);
 }
