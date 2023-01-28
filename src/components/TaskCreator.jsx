67cb8370d9039d94a90ffd055aac0dffb3ec5789
Ernesto De la Rosa <ernestodelazamora@gmail.com>
2023-01-28 11:48:48 -0600

    modified task creator

--- src/components/TaskCreator.jsx
+++ src/components/TaskCreator.jsx
@@ -8,20 +8,26 @@ function TaskCreator({ createNewTask }) {
 		e.preventDefault();
 
 		createNewTask(newTaskName);
-		localStorage.setItem('tasks', newTaskName);
 		setNewtaskName('');
 	};
 
 	return (
 		<div>
-			<form onSubmit={handleSubmit}>
-				<input
-					type='text'
-					placeholder='Enter a new task'
-					value={newTaskName}
-					onChange={e => setNewtaskName(e.target.value)}
-				/>
-				<button>Save Task</button>
+			<form onSubmit={handleSubmit} className='flex flex-wrap pt-6'>
+				<div className='w-3/4'>
+					<input
+						type='text'
+						placeholder='Enter a new task'
+						value={newTaskName}
+						onChange={e => setNewtaskName(e.target.value)}
+						className='mb-1 block w-full appearance-none rounded border border-gray-200 bg-white py-1 px-2 text-base leading-normal text-gray-800'
+					/>
+				</div>
+				<div className='w-1/4'>
+					<button className='inline-block w-full rounded bg-gray-800 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md hover:bg-gray-900 hover:shadow-lg'>
+						Save Task
+					</button>
+				</div>
 			</form>
 		</div>
 	);
